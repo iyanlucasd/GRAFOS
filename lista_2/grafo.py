6a34dd3e026c914bf2951082ed940fc91e55743a
Samir Cambraia <cambraia_samir@hotmail.com>
2021-11-06 19:33:54 -0300

    Toma, meu gostoso. Ta aqui principe

--- lista_2/grafo.py
+++ lista_2/grafo.py
@@ -1,62 +1,62 @@
 class Graph(object):
 
-    def __init__(self, graph_dict=None):
-        """ initializes a graph object 
-            If no dictionary or None is given, 
-            an empty dictionary will be used
+    def __init__(self, grafo_d=None):
+        """ inicializa um objeto gráfico
+            Se nenhum dicionário ou nenhum for fornecido,
+            um dicionário vazio será usado
         """
-        if graph_dict == None:
-            graph_dict = {}
-        self._graph_dict = graph_dict
+        if grafo_d == None:
+            grafo_d = {}
+        self._grafo_d = grafo_d
 
-    def edges(self, vertice):
-        """ returns a list of all the edges of a vertice"""
-        return self._graph_dict[vertice]
+    def limite(self, vertice):
+        """ retorna uma lista de todos os limites de um vértice"""
+        return self._grafo_d[vertice]
         
     def all_vertices(self):
-        """ returns the vertices of a graph as a set """
-        return set(self._graph_dict.keys())
+        """ retorna os vértices de um gráfico como um conjunto"""
+        return set(self._grafo_d.keys())
 
-    def all_edges(self):
-        """ returns the edges of a graph """
-        return self.__generate_edges()
+    def max_limite(self):
+        """ retorna o limite de um gráfico"""
+        return self.__limite_gerado()
 
-    def add_vertex(self, vertex):
-        """ If the vertex "vertex" is not in 
-            self._graph_dict, a key "vertex" with an empty
-            list as a value is added to the dictionary. 
-            Otherwise nothing has to be done. 
+    def add_vertice(self, vertice):
+        """ Se o vértice "vértice" não estiver em
+            self._grafo_d, um "vértice" chave com um vazio
+            lista como um valor é adicionado ao dicionário.
+            Caso contrário, nada precisa ser feito. 
         """
-        if vertex not in self._graph_dict:
-            self._graph_dict[vertex] = []
+        if vertice not in self._grafo_d:
+            self._grafo_d[vertice] = []
 
-    def add_edge(self, edge):
-        """ assumes that edge is of type set, tuple or list; 
-            between two vertices can be multiple edges! 
+    def add_limite(self, limite):
+        """ assume que a borda é do tipo conjunto, tupla ou lista;
+            entre dois vértices pode haver limite múltiplo!
         """
-        edge = set(edge)
-        vertex1, vertex2 = tuple(edge)
-        for x, y in [(vertex1, vertex2), (vertex2, vertex1)]:
-            if x in self._graph_dict:
-                self._graph_dict[x].append(y)
+        limite = set(limite)
+        vertice1, vertice2 = tuple(limite)
+        for x, y in [(vertice1, vertice2), (vertice2, vertice1)]:
+            if x in self._grafo_d:
+                self._grafo_d[x].append(y)
             else:
-                self._graph_dict[x] = [y]
+                self._grafo_d[x] = [y]
 
-    def __generate_edges(self):
-        """ A static method generating the edges of the 
-            graph "graph". Edges are represented as sets 
-            with one (a loop back to the vertex) or two 
-            vertices 
+    def __limite_gerado(self):
+        """ Um método estático que gera o limite do
+            gráfico "gráfico". limite são representados como conjuntos
+            com um (um loop de volta ao vértice) ou dois
+            vértices
         """
-        edges = []
-        for vertex in self._graph_dict:
-            for neighbour in self._graph_dict[vertex]:
-                if {neighbour, vertex} not in edges:
-                    edges.append({vertex, neighbour})
-        return edges
+        limite = []
+        for vertice in self._grafo_d:
+            for vizinho in self._grafo_d[vertice]:
+                if {vizinho, vertice} not in limite:
+                    limite.append({vertice, vizinho})
+        return limite
     
     def __iter__(self):
-        self._iter_obj = iter(self._graph_dict)
+        self._iter_obj = iter(self._grafo_d)
         return self._iter_obj
     
     def __next__(self):
@@ -65,49 +65,49 @@ class Graph(object):
 
     def __str__(self):
         res = "vertices: "
-        for k in self._graph_dict:
+        for k in self._grafo_d:
             res += str(k) + " "
-        res += "\nedges: "
-        for edge in self.__generate_edges():
-            res += str(edge) + " "
+        res += "\nlimite: "
+        for limite in self.__limite_gerado():
+            res += str(limite) + " "
         return res
 
-    def find_path(self, start_vertex, end_vertex, path=None):
-        """ find a path from start_vertex to end_vertex 
-            in graph """
-        if path == None:
-            path = []
-        graph = self._graph_dict
-        path = path + [start_vertex]
-        if start_vertex == end_vertex:
-            return path
-        if start_vertex not in graph:
+    def find_path(self, primeiro_vertice, ultimo_vertice,caminho=None):
+        """ find acaminho from primeiro_vertice to ultimo_vertice 
+            in grafo """
+        ifcaminho == None:
+           caminho = []
+        grafo = self._grafo_d
+       caminho =caminho + [primeiro_vertice]
+        if primeiro_vertice == ultimo_vertice:
+            returncaminho
+        if primeiro_vertice not in grafo:
             return None
-        for vertex in graph[start_vertex]:
-            if vertex not in path:
-                extended_path = self.find_path(vertex, 
-                                               end_vertex, 
-                                               path)
+        for vertice in grafo[primeiro_vertice]:
+            if vertice not incaminho:
+                extended_path = self.find_path(vertice, 
+                                               ultimo_vertice, 
+                                              caminho)
                 if extended_path: 
                     return extended_path
         return None
     
     
-    def find_all_paths(self, start_vertex, end_vertex, path=[]):
-        """ find all paths from start_vertex to 
-            end_vertex in graph """
-        graph = self._graph_dict 
-        path = path + [start_vertex]
-        if start_vertex == end_vertex:
+    def find_all_caminho(self, primeiro_vertice, ultimo_vertice,caminho=[]):
+        """ find all caminho from primeiro_vertice to 
+            ultimo_vertice in grafo """
+        grafo = self._grafo_d 
+       caminho =caminho + [primeiro_vertice]
+        if primeiro_vertice == ultimo_vertice:
             return [path]
-        if start_vertex not in graph:
+        if primeiro_vertice not in grafo:
             return []
-        paths = []
-        for vertex in graph[start_vertex]:
-            if vertex not in path:
-                extended_paths = self.find_all_paths(vertex, 
-                                                     end_vertex, 
-                                                     path)
-                for p in extended_paths: 
-                    paths.append(p)
-        return paths
+        caminho = []
+        for vertice in grafo[primeiro_vertice]:
+            if vertice not incaminho:
+                extended_caminho = self.find_all_caminho(vertice, 
+                                                     ultimo_vertice, 
+                                                    caminho)
+                for p in extended_caminho: 
+                    caminho.append(p)
+        return caminho
